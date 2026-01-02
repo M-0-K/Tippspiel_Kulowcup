9bada7f04650fc8b843686e18fed791c5adeda5e
M0k <mkockert@hs-mittweida.de>
2026-01-03 00:27:10 +0100

    Add Website Werbung

--- html/default/footer.php
+++ html/default/footer.php
@@ -1,3 +1,11 @@
+	<footer id="site-footer">
+		<div id="presentation-footer-hint">
+			Tippen & Live-Daten unter
+			<a href="https://kulowcup.win" target="_blank" rel="noopener noreferrer">https://kulowcup.win</a>
+            
+		</div>
+	</footer>
+
 </body>
 
 </html>
